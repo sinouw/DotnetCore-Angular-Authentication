af49242e3da44eba9b357fd5f9254c42a3f5c579
Yassine El Ouni <yassine.ouni001@gmail.com>
2019-08-03 20:39:09 +0100

    firstcommit

---
+++ WebAPI/WebAPI/Models/Auth/EshopContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class EshopContext : IdentityDbContext
+    {
+        public EshopContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        // Creating Roles for the application
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>().HasData(
+                    new { Id = "1", Name = "SuperAdmin", NormalizedName = "SUPERADMIN" },
+                    new { Id = "2", Name = "Admin", NormalizedName = "ADMIN" }
+                );
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
